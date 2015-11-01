0638a17f362d0100d51b9039f29da4575c599681
cristi <cristi@kth.se>
2015-11-01 13:15:35 +0100

    export surface_survey, ref

--- smoothness-trunk.sql
+++ smoothness-trunk.sql
@@ -2,9 +2,9 @@ SELECT row_to_json(fc)
  FROM
   ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
   FROM (SELECT 'Feature' As type,
-      ST_AsGeoJSON(ST_Transform(lg.way, 4326))::json As geometry,
+      ST_AsGeoJSON(ST_Transform(ST_simplify(lg.way, 50), 4326))::json As geometry,
           (
-	          select row_to_json(t) from (select smoothness, highway) t
+	          select row_to_json(t) from (select smoothness, highway, surface_survey, ref) t
 	   ) AS properties
 			     FROM planet_osm_roads As lg WHERE lg.smoothness is not null and lg.highway in ('trunk', 'motorway')
    ) As f
