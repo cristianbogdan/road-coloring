ccd4796b94245c84d777689dd1549f0fcf65dec5
cristi <cristi@kth.se>
2017-01-27 23:19:21 +0100

    even better search

--- search/search.sql
+++ search/search.sql
@@ -1,11 +1,11 @@
---SET client_encoding = 'UTF8';
+SET client_encoding = 'UTF8';
 DROP function search(text);
 CREATE FUNCTION search (text) RETURNS text as $$
 SELECT row_to_json(fc)::text
  FROM
   ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
   FROM (SELECT 'Feature' As type,
-      ST_AsGeoJSON(ST_Transform(ST_simplify(lg.way, 50), 4326))::json As geometry,
+      ST_AsGeoJSON(ST_Transform(lg.way, 4326))::json As geometry,
           (
 	          select row_to_json(t) from
 		  (select
@@ -16,9 +16,11 @@ SELECT row_to_json(fc)::text
 		  ) t
 	   ) AS properties
 			     FROM(
-			     (SELECT way as way, place as type FROM planet_osm_polygon WHERE unaccent_string($1) in (unaccent_string(name), unaccent_string(ref)))
+			     (SELECT way as way,  concat('0',place) as type FROM planet_osm_polygon WHERE unaccent_string($1) = unaccent_string(name) and place is not null)
 			     union
-			      (SELECT ST_union(way) as way, 'road' as type FROM planet_osm_line WHERE $1 in (osm_id::text, name, ref)) 
+			     (SELECT way as way, concat('1',place) as type FROM planet_osm_point WHERE unaccent_string($1)= unaccent_string(name) and place is not null)
+			     union
+			    (SELECT ST_union(way) as way, concat('2',highway) as type FROM planet_osm_line WHERE highway is not null and $1 in (osm_id::text, name, ref) group by highway) 
 
 			     )
 as lg 
