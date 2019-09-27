261f3c45eacc406a2083f4847ce4c3290f7e0cde
Cristi <cristi@kth.se>
2019-09-27 22:57:04 +0300

    containe TERM signal on docker stop, db orderly shutdown

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo chown -R postgres.postgres /var/lib/postgresql/
 sudo chown -R cristi.cristi /tmp/stache
 sudo chown -R cristi.cristi /home/cristi/maps/data
@@ -13,6 +12,22 @@ service nginx start
 #start cron
 service cron start
 
+
+cleanup()
+{
+    echo "Caught Signal ... cleaning up."
+    service postgresql stop
+    echo "Done cleanup ... quitting."
+    exit 0
+    }
+
+trap cleanup INT TERM
+
 #make sure the container stays busy so it does not exit
-cat
+coproc read  && wait "$!" || true
+
+while :; do
+    echo "Hello! ${SECONDS} secs elapsed..."
+
+    done
 
