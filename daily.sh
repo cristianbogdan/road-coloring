f0ce99ae03b3c7a6f8fff744abf3909f5504f878
Cristi <cristi@kth.se>
2020-12-09 11:27:59 +0200

    separate error, no project map rendering

--- daily.sh
+++ daily.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
-# install: postgress, postgis, osm2pgsql, 
-
-# create gis db:
-# createdb -E UTF-8 -O USERNAME gis
-# run postgis SQL scripts, paths on your system may differ:
-# psql -d gis -f /usr/local/share/postgis/postgis.sql
-# psql -d gis -f /usr/local/share/postgis/spatial_ref_sys.sql
-# psql -d gis -c "CREATE EXTENSION postgis"
-# psql -d gis -c "CREATE EXTENSION hstore"
-
-# npm install -g topojson
-
-# brew install python
-# easy_install pip
-# sudo pip install tilestache
-# sudo pip install mapnik and all other tilestache dependencies
-# sudo apt-get install python-shapely
-# sudo pip install mapbox-vector-tile
-# sudo pip install psycopg2-binary
-# sudo pip install osmapi
-# define functions search() and unaccent_string()
-# copy tilestache/template/* or generate them with tilestache-list (see below)
-
-#brew install nginx --with-passenger --with-gzip
-# follow Caveats instructions "to activate passenger"...
-# on ubuntu follow https://www.phusionpassenger.com/library/install/nginx/install/oss/xenial/
+# see docker/Dockerfile
 
 # run daily
 
@@ -40,7 +15,7 @@ if [ $(stat -c%s $OUTPUT) -gt 5000 ] ; then
     mv $OUTPUT data/data-roads.osm.pbf
 else
     cat data/err.txt
-    mv data/err.txt $(mktemp data/error_XXXXXX)
+    mv data/err.txt $(mktemp data/error-roads_XXXXXX)
     exit 1;
 fi
 
@@ -76,14 +51,8 @@ rm -rf /tmp/stache/tiles/13 /tmp/stache/tiles/14  /tmp/stache/tiles/15  /tmp/sta
 
 echo `date` generating vector tiles for zoom levels 7 to 12
 #ls -1 template/712roads-* | xargs -n1 -P4 tilestache-seed.py -c tilestache.cfg -x -e json -l roads -q --tile-list
-#ls -1 template/712-* | xargs -n1 -P4 tilestache-seed.py -c tilestache.cfg -x -e json -l roads -q --tile-list
 ls -1 template/712-* | xargs -n1 -P4 tilestache-seed.py -c tilestache.cfg -x -e json -l roads -q --tile-list
 rm -rf /tmp/stache/roads/13 /tmp/stache/roads/14 /tmp/stache/roads/15 /tmp/stache/roads/16 /tmp/stache/roads/17 /tmp/stache/roads/18
-#rm -rf /tmp/stache/roads/13 /tmp/stache/roads/14 /tmp/stache/roads/15 /tmp/stache/roads/16 /tmp/stache/roads/17 /tmp/stache/roads/18
-
-echo `date` generating raster tiles for opening_date, zoom levels 7 to 12
-ls -1 template/712-* | xargs -n1 -P4 tilestache-seed.py -c tilestache.cfg -x -e png -l infra -q --tile-list
-rm -rf /tmp/stache/infra/13 /tmp/stache/infra/14 /tmp/stache/infra/15 /tmp/stache/infra/16 /tmp/stache/infra/17 /tmp/stache/infra/18
 
 echo `date` finished
 
