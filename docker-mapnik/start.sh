fa908ffce63744fc4ac69e8ec1418909e71deb9e
cristianbogdan <cristi@kth.se>
2022-06-02 08:52:03 +0000

    ensuring logging in stdout and stderr to be picked by docker

--- docker-mapnik/start.sh
+++ docker-mapnik/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 chmod -R a+rw /tmp/stache
-trap "/work/maps/road-quality/osmimport/prerender-quality.sh 2>1 >>/data/daily.log" USR1
-trap "/work/maps/infra-projects/osmimport/prerender-projects.sh" USR2
+trap "/work/maps/road-quality/osmimport/prerender-quality.sh" USR1
+trap "/work/maps/infra-projects/osmimport/prerender-infra.sh" USR2
 
-nginx &
+#nginx &
 
-while
-    /bin/true ; do
-    sleep 1
-done
+#while
+#    /bin/true ; do
+#    sleep 1
+#done
 
-#nginx -g "daemon off;"
+nginx -g "daemon off;"
