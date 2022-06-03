e89421e4729354ba9d2ee7e674e6779d666c34ea
cristianbogdan <cristi@kth.se>
2022-06-03 04:56:42 +0000

    reverting for traps to work

--- docker-mapnik/start.sh
+++ docker-mapnik/start.sh
@@ -3,11 +3,11 @@ chmod -R a+rw /tmp/stache
 trap "/work/maps/road-quality/osmimport/prerender-quality.sh" USR1
 trap "/work/maps/infra-projects/osmimport/prerender-infra.sh" USR2
 
-#nginx &
+nginx &
 
-#while
-#    /bin/true ; do
-#    sleep 1
-#done
+while
+    /bin/true ; do
+    sleep 1
+done
 
-nginx -g "daemon off;"
+#nginx -g "daemon off;"
