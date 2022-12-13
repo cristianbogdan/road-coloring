795bc1d8fd4eceffeda056f262be0fa52a774240
cristi <cristi@kth.se>
2022-12-13 01:01:32 +0100

    moved templates to /tmp/stache/template

--- road-quality/osmimport/prerender-quality.sh
+++ road-quality/osmimport/prerender-quality.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # executed in the docker-mapnik after a USR1 signal
 
-# this is how to generate the template folders
-# tilestache-list.py  -b 48 20 43 29 7 8  9 10 11 12  | split -l 200 - template/712-
+mkdir /tmp/stache/template
+tilestache-list  -b 48 20 43 29 7 8  9 10 11 12  | split -l 200 - /tmp/stache/template/712-
+
 cd /work/maps/road-quality/tilestache
 
 echo "---- removing raster tiles ----"
@@ -10,7 +11,7 @@ mv /tmp/stache/tiles /tmp/stache/tiles-temp
 rm -rf /tmp/stache/tiles-temp
 
 echo "---- generating raster tiles for zoom levels 7 to 12 ---"
-ls -1 template/712-* | xargs -n1 -P4 tilestache-seed.py -c quality.cfg -l tiles -e png -q -x --tile-list 
+ls -1 /tmp/stache/template/712-* | xargs -n1 -P4 tilestache-seed.py -c quality.cfg -l tiles -e png -q -x --tile-list 
 #rm -rf /tmp/stache/tiles/13 /tmp/stache/tiles/14  /tmp/stache/tiles/15  /tmp/stache/tiles/16  /tmp/stache/tiles/17  /tmp/stache/tiles/18  
 
 #ls -1 template/715-* | xargs -n1 -P4 tilestache-seed.py -c tilestache.cfg -l tiles -e png -q -x --tile-list 
@@ -22,7 +23,7 @@ mv /tmp/stache/roads /tmp/stache/roads-temp
 rm -rf /tmp/stache/roads-temp
 
 echo "---- generating vector tiles for zoom levels 7 to 12 ----"
-ls -1 template/712-* | xargs -n1 -P4 tilestache-seed.py -c edit.cfg -x -e json -l roads -q --tile-list
+ls -1 /tmp/stache/template/712-* | xargs -n1 -P4 tilestache-seed.py -c edit.cfg -x -e json -l roads -q --tile-list
 #rm -rf /tmp/stache/roads/13 /tmp/stache/roads/14 /tmp/stache/roads/15 /tmp/stache/roads/16 /tmp/stache/roads/17 /tmp/stache/roads/18
 
 echo finished
