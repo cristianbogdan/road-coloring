ca24adcf292f6fe36037f09d4ab1091c8d744228
cristianbogdan <cristi@kth.se>
2022-12-18 03:22:46 +0000

    a failed (so far) attempt to convert GeoJSON to VT, then to PBF, for testing VectorGrid.protobuf

--- infra-projects/osmimport/json2pbf.sh
+++ infra-projects/osmimport/json2pbf.sh
@@ -1,6 +1,8 @@
-# pip install geobuf
+# attempt to convert GeoJSON tiles to PBF. does not seem to work well so far
+# see json-vt-pbf.js
 # cp -r /tmp/stache/infraVector /tmp/stache/infraPbf
-# find /tmp/stache/infraPbf -name *.json -exec ./json2pbf.sh {} \;
+# find infraPbf -name "*.json" -exec ./convert.sh {} \;
 
-geobuf encode < $1 >`dirname "$1"`/`basename "$1" .json`.pbf  && rm $1
+echo $1
+node json-vt-pbf.js < $1 >`dirname "$1"`/`basename "$1" .json`.pbf  && rm $1
 
