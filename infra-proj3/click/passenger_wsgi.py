8a01c6c390587b66485a71b120f8d565d6013536
cristianbogdan <cristi@kth.se>
2022-12-20 17:44:45 +0000

    click endpoint with "latitude,longitude"

---
+++ infra-proj3/click/passenger_wsgi.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python
+import sys
+from cgi import parse_qs, escape
+import traceback
+import psycopg2
+import sys
+import urllib2
+import urllib
+import json
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+def application(environ, start_response):
+    status = '200 OK'
+    progress=''
+    d = parse_qs(environ['QUERY_STRING'])
+
+    try:
+        latLng = d.get('latLng')[0]
+        comma= latLng.index(",")
+        lat= float(latLng[:comma].strip())
+        lon= float(latLng[comma+1:].strip());
+
+        conn=psycopg2.connect('dbname=gis1 user=postgres')
+        cursor=conn.cursor()
+        cursor.execute(
+            "select row_to_json(t)::text from (select  osm_id, ref, name, highway, railway, construction, status, proposed, start_date, maxspeed, opening_date, st_distance(way, st_transform(ST_setSRID(ST_point("+str(lon)+","+str(lat)+" ), 4326), 900913)) as distance from planet_osm_line1 order by distance asc limit 1) t"
+            ,())
+        if(cursor.rowcount==0):
+            response_body="{}"
+        else:
+            response_body= cursor.fetchone()[0]
+    except Exception as inst:
+        traceback.print_exc()
+        status= '400 Bad request'
+        response_body=''.join(["{","\"error\": \"", str(inst).replace('\n',';'), "\"}"])
+    
+    
+    response_headers = [('Content-type', 'application/json;charset=utf-8'),
+                    ('Content-Length', str(len(response_body)))]
+    start_response(status, response_headers)
+    return [response_body]
+
+
