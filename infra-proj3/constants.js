fed197a617171d4cc4d4b988c6f380ad929f55f2
Theo Constantin <b.theo95@gmail.com>
2022-12-23 01:38:02 +0200

    add constants.js

---
+++ infra-proj3/constants.js
@@ -0,0 +1,25 @@
+const Color = {
+    RED: '#ff0000',
+    ORANGE: '#ff8000',
+    GRAY: '#808080',
+    POWDER_BLUE: '#b0e0e6',
+    LIGHT_SKY_BLUE: '#87cefa',
+    DEEP_SKY_BLUE: '#00bfff',
+    ORANGE_RED: '#ff4500',
+    DODGER_BLUE: '#1e90ff',
+}
+
+const zoomPrecisionMap = {
+    7: 8000,
+    8: 4000,
+    9: 2000,
+    10: 1000,
+    11: 500,
+    12: 300,
+    13: 200,
+    14: 100,
+    15: 50,
+    16: 25,
+    17: 15,
+    18: 10
+}
