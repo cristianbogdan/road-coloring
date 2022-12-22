8a72a7fc9db83d7dbdec0e6f6982a110c88733bd
Theo Constantin <b.theo95@gmail.com>
2022-12-23 01:38:53 +0200

    implement popup for map click

--- infra-proj3/tiles-vt.js
+++ infra-proj3/tiles-vt.js
@@ -15,15 +15,153 @@
   </head>
 */
 
+//checks if any element from within a list is a key in an object
+function jsonHasAnyKey(obj, list) {
+    for (const element of list) {
+        if (element in obj) {
+            return true;
+        }
+    }
+}
+
 try { MAP_ROOT }catch(e) {
     MAP_ROOT="";
+}
+var map = undefined;
+
+function computeStatus(props) {
+    props.status.split(',').forEach(function (prop) {
+        var kv = prop.split(':');
+        if (kv.length > 1) {
+            props[kv[0]] = kv[1];
+            if (kv[0] == 'progress') {
+                props.progress = kv[1].split(' ');
+                props.latestProgress = parseFloat(props.progress[0].split('%')[0]);
+            } else if (kv[0] == 'progress_estimate') {
+                props.progress_estimate = kv[1].split(' ');
+                props.latestProgress = parseFloat(props.progress_estimate[0].split('%')[0]);
+            } else if (kv[0] == 'signal_progress') {
+                props.signal_progress = kv[1].split(' ');
+                props.latestSignalProgress = parseFloat(props.signal_progress[0].split('%')[0]);
+            }
+        } else
+            props[kv[0]] = true;
+    });
+    //if(p.progress_estimate)
+    //p.latestProgress=parseFloat(p.progress_estimate.split('%')[0]);
+    props.hadStatus = true;
+    props.status = null;
+};
+
+function getPopupHtmlContent(props) {
+	if (!props.osm_id) props.osm_id=props.id.split('/')[1];
+	
+    if (props.comentarii_problema) {
+        return '<b>' + props.nume + '</b><br/>'
+            + props.comentarii_problema + '<br/><br/>'
+            + props.comentarii_rezolvare_curenta + '<br/>'
+            + 'Estimare: ' + props.estimare_rezolvare +
+            (props.link ? ('<br/><a href="' + props.link + '" target="PUM">detalii</a>') : '');
+
+
+    }
+    if (props.highway == 'lot_limit' || props.railway == 'lot_limit')
+        return 'Limita lot ' + (props.highway ? 'autostrada' : 'CF') + ' <a href=\"https://openstreetmap.org/node/' + props.osm_id + '\" target="OSM">' + props.name + '</a>';
+
+    var x = (props.highway ? props.highway : props.railway)
+        + ' <a href=\"https://openstreetmap.org/way/' + props.osm_id + '\" target="OSM">'
+        + (props.ref ? props.ref + (props.name ? ('(' + props.name + ')') : '') : (props.name ? props.name : props.osm_id))
+        + "</a>"
+        //+"[<a href=\"https://openstreetmap.org/edit?way="+prop.osm_id+"\" target=\"OSMEdit\">edit</a>] "
+        //    +"</a> [<a href=\"https://openstreetmap.org/edit?editor=potlatch2&way="+prop.osm_id+"\" target=\"OSMEdit\">edit-potlach</a>]"
+    ;
+
+    if (props.status) computeStatus(props);
+
+    if (props.highway == 'construction' || props.highway == 'proposed' || (props.railway && props.latestProgress != 100)) {
+        x += (props.opening_date ? "<br>Estimarea terminarii constructiei: " + props.opening_date : '');
+        x += (props.access == 'no' ? "<br><font color='red'>Inchis traficului la terminarea constructiei</font>" : '');
+
+        if (props.hadStatus)
+            if (props.highway) x += "<br>" + (props.AC ? '<font color=' + Color.DEEP_SKY_BLUE + '>Autorizatie de construire</font>' : props.PTE ? '<font color=' + Color.ORANGE + '>Are Proiect Tehnic aprobat dar nu Autorizatie de Construire</font>' : props.AM ? '<font color=' + Color.ORANGE_RED + '>Are Acord de Mediu dar nu Proiect Tehnic aprobat, deci nu are Autorizatie de Construire</font>' : '<font color=' + Color.RED + '>Nu are Acord de Mediu, deci nu are Autorizatie de Construire</font>');
+            else x += (props.AC ? "<br>" + '<font color=' + Color.DEEP_SKY_BLUE + '>Autorizatie de construire</font>' : '');
+        else if (props.highway)
+            x += "<br>Progresul constructiei necunoscut";
+        if (props.tender) {
+            x += "<br>In licitatie " + props.tender;
+            if (props.winner) x += "<br> castigator " + props.winner;
+        }
+        x += (props.builder ? "<br>Constructor: " + props.builder : '');
+        x += (props.severance ? "<br>Reziliat: " + props.severance : '');
+        x += (props.funding ? "<br>Finantare: " + props.severance : '');
+
+        if (props.progress) {
+            var color = props.latestProgress > 75 ? Color.DODGER_BLUE : props.latestProgress > 50 ? Color.DEEP_SKY_BLUE : props.latestProgress > 25 ? Color.LIGHT_SKY_BLUE : props.latestProgress > 0 ? Color.POWDER_BLUE : Color.GRAY;
+            x += "<br>Stadiul lucrarilor: <font color=" + color + "><b>" + props.progress[0] + "</b></font><font size=-2>"
+                + props.progress.slice(1).reduce(function (s, e) {
+                    return s + " " + e.trim();
+                }, "")
+                + "</font>";
+        }
+        if (props.progress_estimate) {
+            var color_e = props.latestProgress > 75 ? Color.DODGER_BLUE : props.latestProgress > 50 ? Color.DEEP_SKY_BLUE : props.latestProgress > 25 ? Color.LIGHT_SKY_BLUE : props.latestProgress > 0 ? Color.POWDER_BLUE : Color.GRAY;
+            x += "<br>Estimare stadiu: <font color=" + color_e + "><b>" + props.progress_estimate[0] + "</b></font><font size=-2>"
+                + props.progress_estimate.slice(1).reduce(function (s, e) {
+                    return s + " " + e.trim();
+                }, "")
+                + "</font>";
+        }
+    } else {
+        if (props.highway) {
+            x += (props.start_date ? "<br>Data terminarii constructiei: " + props.start_date : '');
+            x += (props.opening_date ? "<br>Dat in circulatie: " + props.opening_date : "");
+
+        } else if (props.railway) {
+            x += (props.start_date ? "<br>Data terminarii variantei noi: " + props.start_date : '');
+            x += (props.opening_date ? "<br>Data terminarii reabilitarii: " + props.opening_date : '');
+        }
+        x += props.access == 'no' ? "<br><font color='red'>Inchis traficului</font>" : "";
+    }
+
+    if (props.railway) {
+        if (props.signal_progress && !props["railway:etcs"]) {
+            x += "<br>Semnalizare ETCS: <font color=" + Color.ORANGE + "><b>" + props.signal_progress[0] + "</b></font><font size=-2><br>"
+                + props.signal_progress.slice(1).reduce(function (s, e) {
+                    return s + " " + e.trim();
+                }, "")
+        } else if (props["railway:etcs"]) x += "<br>Semnalizare ETCS: nivel " + props["railway:etcs"];
+        else if (props["construction:railway:etcs"]) x += "<br>Semnalizare ETCS: implementare impreuna cu reabilitarea liniei, nivel " + props["construction:railway:etcs"];
+        else x += "<br>Semnalizare ETCS: neimplementat";
+    }
+
+    x += props.bridge == 'yes' ? "<br>Pod" : "";
+    x += props.tunnel == 'yes' ? "<br>Tunel" : "";
+    return x;
+}
+
+
+function showPopupDetails(latlng, props) {
+    L.popup()
+        .setLatLng(latlng)
+        .setContent(getPopupHtmlContent(props))
+        .openOn(map);
+}
+
+function mapClick(event) {
+    const { lat, lng } = event.latlng;  
+    const latlngStr = `${lat},${lng}`;
+    console.log(zoomPrecisionMap[map.getZoom()])
+    fetch(`${MAP_ROOT}/click?latLng=${latlngStr}&max=${zoomPrecisionMap[map.getZoom()]}`).then(response => response.json()).then(data => {
+        if (data === null) return
+        showPopupDetails(event.latlng, data);
+    });
 }
 
 function loadDoc(zoom) {
     zoom=zoom||7;
     
 
-    var map= new L.Map('mymap',{
+    map= new L.Map('mymap',{
         minZoom:7,
         maxZoom:18
     });
@@ -102,6 +240,12 @@ function loadDoc(zoom) {
         weight: 3.0,
     };
 
+    const underConstructionHighway = {
+        clickable: true,
+        color: Color.DEEP_SKY_BLUE, 
+        weight: 3.0,
+    };
+
     var hoverStyle = {
         "fillOpacity": 0.5
     };
@@ -114,9 +258,10 @@ function loadDoc(zoom) {
         solidChildren: true,
         style(feature){
             const tags = feature.tags;
-            if (feature.type == 1) console.log(feature.type, tags)
+            // if (feature.type == 1) console.log(feature.type, tags)
             
             if (tags.highway === 'proposed') return proposedHighwayStyle;
+            if (tags.highway === 'construction') return underConstructionHighway;
             if(['motorway', 'primary', 'trunk', 'rest_area'].includes(tags.highway)) return highwayStyle;   
 
             if(tags.highway === 'lot_limit' || tags.railway === 'lot_limit') return invisibleStyle;
@@ -132,45 +277,15 @@ function loadDoc(zoom) {
             return defaultStyle;
         },
         filter: function(feature, layer) {
-            // return false;
-            if (!feature.tags) return true;
-            // id: "node/326930529"
-            // information: "guidepost"
-            // tourism: "information"
-            // {abandoned: 'yes', disused:railway: 'station', id: 'node/243742782'}
-            
-            const { railway, highway, amenity, public_transport, tourism, traffic_sign } = feature.tags;
-            // return true;
-            return !(['motorway_junction', 'crossing', 'give_way', 'traffic_signals', 'stop', 'milestone'].includes(highway)
-            || ['level_crossing', 'halt', 'crossing', 'station', 'tram_level_crossing', 'switch', 'buffer_stop', 'signal', 'railway_crossing'].includes(railway) // tram_level_crossing
-            || jsonHasAnyKey(feature.tags, ['amenity', 'public_transport', 'tourism', 'traffic_sign',
-             'traffic_calming', 'barrier', 'disused:railway' ,'noexit', 'power', 'traffic_sign:forward', 'crossing',
-            'man_made', 'emergency', 'border', 'entrance']));
-
-            // return feature.geometry.type=="LineString" || feature.geometry.type=="Polygon" || feature.properties.railway=="lot_limit" || feature.properties.highway=="lot_limit"  ;
+            return true;
         }
     };
-
-    //checks if any element from within a list is a key in an object
-    function jsonHasAnyKey(obj, list) {
-        for (const element of list) {
-            if (element in obj) {
-                return true;
-            }
-        }
-    }
 
     fetch("/maps/data/data-sql-infra.geo.json").then(r=>r.json()).then(function(data){
         const layer= L.geoJson.vt(data, options);
         layer.addTo(map);
         layer.bindPopup('Hi There!');
-        map.on('click', function (e) {
-            console.log(e.latlng);
-            L.popup()
-                .setLatLng(e.latlng)
-                .setContent('<p>'+e.latlng+'</p>')
-                .openOn(map);
-        });
+        map.on('click', mapClick);
 
 
         
