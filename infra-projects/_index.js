d8847cc724f9c955fb80a11a0782b131472e7869
cristianbogdan <cristi@kth.se>
2023-01-08 21:10:24 +0000

    moved tiles-vt to infra-projects

--- infra-projects/_index.js
+++ infra-projects/_index.js
@@ -1,50 +1,37 @@
-SCRIPT_ROOT+="/infra-projects/";
+SCRIPT_ROOT+="/infra-proj3/";
 document.title='Harta proiectelor de infrastructură din România';
 
 var yr= new Date().getFullYear();
 
 document.head.insertAdjacentHTML
 ( 'afterbegin','<style>'
++  'body {            padding: 0;            margin: 0;}'
++ 'html,     body,        #mymap {            height: 100%;            width: 100%;        }'
 +'body,html{height:100%;width:100%;}'
-+'.map {  height:100%;width:100%;}'
-+'.popover{width:300px;}'
-+'div.ol-attribution button{position: absolute; bottom: 12px;right: 0;  }'
-  +'#text{  position:absolute; left:50%; bottom:0px;}'
-  +'#api{  position:absolute; right:10px; top:10px; background-color:rgba(255, 255, 255, 0.4);}'
 +'</style>'
 );
 
 function insertHTML(){
 document.body.insertAdjacentHTML
-( 'afterbegin',
-  '<div id="map" class="map"> '
-+'<div id="gmap" class="map"></div>'
-  +'<div id="olmap" class="map"><div id="popup"></div></div>'
-  +'</div>'
-  +'  <div id="text"></div>'
-  +'  <a href="https://proinfrastructura.ro"><img id="api" src="https://proinfrastructura.ro/images/logos/api/logo_api_portrait_big.png" width="50"/></a>'
-
-
-)  ;
+    ( 'afterbegin', '    <div id="mymap"></div>');
 }
 
 loadScript(["../common/googleMapsKey.js", loadAll],0);
 
 function loadAll(){
-loadScript([
-    "https://unpkg.com/jquery@1.11.2/dist/jquery.min.js",
-    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css", 
-    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js",
-    insertHTML,
-//    "https://cdnjs.cloudflare.com/ajax/libs/openlayers/4.6.5/ol.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/openlayers/4.6.5/ol.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/openlayers/3.15.1/ol.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/openlayers/3.15.1/ol.js",
-//    "https://tyrasd.github.io/osmtogeojson/osmtogeojson.js",
-    "https://maps.google.com/maps/api/js?v=3&sensor=false&key="+window.googleMapsKey,
-    "../common/gmap.js",
-    "project-colors.js",
-    "projects-legend.js",
-    "vector-gmaps.js"
+    loadScript([
+        "leaflet-1.9.3.css",
+        "style.css",
+        "leaflet-1.9.3.js",
+        "https://unpkg.com/geojson-vt@3.2.0/geojson-vt.js",
+        "leaflet-geojson-vt/index.js",
+        "constants.js",
+        "legend.js",
+        "../common/thunderforestKey.js",
+        "leaflet.edgebuffer.js",
+        "tiles-vt.js",
+        insertHTML,
+        ()=>loadDoc(8)
 ], 0);
 }
+
