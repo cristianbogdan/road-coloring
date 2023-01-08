64b2b7a4e7c53b2d0797843b530f94594bed1385
Theo Constantin <b.theo95@gmail.com>
2023-01-08 02:42:34 +0200

    add show legend button

--- infra-proj3/legend.js
+++ infra-proj3/legend.js
@@ -1,3 +1,10 @@
+const ShowLegendButtonText = {
+    SHOW: ">",
+    HIDE: "<"
+}
+let isLegendVisible = true;
+let showLegendButtonText = ShowLegendButtonText.SHOW;
+
 const contributors =
     '<div style="text-align:left;line-height:115%;">'
     + '© <a href="https://openstreetmap.org">OpenStreetMap</a> contributors<br>'
@@ -5,25 +12,40 @@ const contributors =
     + '</div>';
 
 const legendText =
-    '<div style="position:relative; display:inline-block; font-size:12px; font-weight:bold; color:blue;">2023</div> - Deschidere (estimată)<br>'
+    '<div class="legend-content-element">'
+    + '<div style="position:relative; display:inline-block; font-size:12px; font-weight:bold; color:blue;">2023</div> - Deschidere (estimată)<br>'
     + '<div style="position:relative; display:inline-block; font-size:12px; font-weight:bold; color:red;">2023</div> - Deschidere fără acces<br>'
-    + 'AC - Autorizație de Construire<br>PT - Proiect Tehnic<br>AM - Acord de Mediu<br>'
+    + 'AC - Autorizație de Construire<br>PT - Proiect Tehnic<br>AM - Acord de Mediu'
+    + '</div>'
 
 const getInvolved = '<a href=http://forum.peundemerg.ro/index.php?topic=836.msg161436#msg161436>Get involved!</a><br>';
 
 const filters =
-    '<div onclick="legendFilterClickHandler(event, this)">'
+    '<div class="legend-content-element" onclick="legendFilterClickHandler(event, this)">'
     + legend.projectTypes.map(x => '<span>' + (x.canHide ? '<input type=checkbox' + (x.hidden ? '' : ' checked ') + '> ' : ' ') + '<div style="' + legend.basicStyle + ' ' + x.symbol + '"></div> ' + x.text + "<br></span>").join('')
     + '</div>';
 
+const showLegendButton = `<button id="show-legend-button" onclick="showLegendClicked()" class="show-legend-button">${showLegendButtonText}</button>`;
+
+function showLegendClicked() {
+    isLegendVisible = !isLegendVisible;
+    const showLegendButtonText = isLegendVisible ? ShowLegendButtonText.SHOW : ShowLegendButtonText.HIDE;
+
+    const showLegendButton = document.getElementById("show-legend-button");
+    showLegendButton.innerHTML = showLegendButtonText;
+
+    const legendContentElements = document.getElementsByClassName("legend-content-element");
+    for (const legendContentElement of legendContentElements) {
+        legendContentElement.style.display = isLegendVisible ? "contents" : "none";
+    }
+}
+
 function legendContent() {
-    return filters + legendText;
+    return filters + legendText + showLegendButton;
 }
 
 const legendFilterClickHandler = (e, div) => {
-    if (e.target.type !== "checkbox")
-        return;
-    e.stopPropagation();
+    if (e.target.type !== "checkbox") return;
     const indexFilter = [...div.children].findIndex(x => x.firstChild === e.target);
     legend.projectTypes[indexFilter].hidden = !e.target.checked;
     roadsLayer.reinitialize();
