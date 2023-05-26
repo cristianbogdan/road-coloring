6435d82368bb69a5f3b33b2d94559105413db5ad
Theo Constantin <b.theo95@gmail.com>
2023-05-27 02:27:18 +0300

    fix data-processing types

--- infra-projects/client/src/data-processing.ts
+++ infra-projects/client/src/data-processing.ts
@@ -1,28 +1,41 @@
-import { Props } from "./road-style";
+import type { Props } from "./types";
 
 export function computeStatus(props: Props) {
     if (!props.status) return;
     for (const prop of props.status.split(',')) {
         const [prop_key, prop_value] = prop.split(':');
-        if (prop_value) {
-            props[prop_key] = prop_value;
-            if (prop_key == 'progress') {
-                props.progress = prop_value.split(' ');
-                props.latestProgress = parseFloat(props.progress[0].split('%')[0]);
-            } else if (prop_key == 'progress_estimate') {
-                props.progress_estimate = prop_value.split(' ');
-                props.latestProgress = parseFloat(props.progress_estimate[0].split('%')[0]);
-            } else if (prop_key == 'signal_progress') {
-                props.signal_progress = prop_value.split(' ');
-                props.latestSignalProgress = parseFloat(props.signal_progress[0].split('%')[0]);
-            }
+
+        // props[prop_key] = prop_value;
+        if (prop_key == 'progress') {
+            props.progress = prop_value.split(' ');
+            props.latestProgress = parseFloat(props.progress[0].split('%')[0]);
+        } else if (prop_key == 'progress_estimate') {
+            props.progress_estimate = prop_value.split(' ');
+            props.latestProgress = parseFloat(props.progress_estimate[0].split('%')[0]);
+        } else if (prop_key == 'signal_progress') {
+            props.signal_progress = prop_value.split(' ');
+            props.latestSignalProgress = parseFloat(props.signal_progress[0].split('%')[0]);
+        } else if (prop_key == 'tender') {
+            props.tender = prop_value;
+        } else if (prop_key == 'builder') {
+            props.builder = prop_value;
+        } else if (prop_key == 'severance') {
+            props.severance = prop_value;
+        } else if (prop_key == 'financing') {
+            // TODO: this is misspelled with 73 times
+            props.financing = prop_value;
+        } else if (prop_key == 'winner') {
+            props.winner = prop_value;
+        } else if (prop_key == 'PTE') {
+            props.PTE = true;
+        } else if (prop_key == 'AM') {
+            props.AM = true;
+        } else if (prop_key == 'AC') {
+            props.AC = true;
         } else {
-            // PTE, AM, AC    
-            props[prop_key] = true;
+            // console.warn('Unknown feature status key: ' + prop_key);
         }
     };
-    //if(p.progress_estimate)
-    //p.latestProgress=parseFloat(p.progress_estimate.split('%')[0]);
     props.hadStatus = true;
-    props.status = null;
+    props.status = undefined;
 };
